0965b8aa3cde3e3de116244f99036d37a46dedca
isalemi-rgb <isalemi@iu.edu>
2026-09-21 19:39:58 -0400

    9/21/26_7:40_final

--- app.js
+++ app.js
@@ -1,60 +1,63 @@
-console.log("Hello World!")
+const name = "Isabella Salemi"
+const string1 = "Hello World! My name is "
+const string2 = ", and what's your name?"
+let username = prompt(string1 + name + string2)
 
-//var no good no mo? ;{
-//const is a variable that can never change, its constant and never changing, and var is better
-//const age = 32;
+console.log("Hello, " + username + "!")
+
+console.log("Today it is " + new Date() + "!")
+
+
+let num1 = 0
+let num2 = 0
+let num3 = 0
+let add = 0
+let average = 0
+
+num1 = prompt("Please enter number 1")
+num2 = prompt("Please enter number 2")
+num3 = prompt("Please enter number 3")
+
+console.log("Your numberss weree......")
+
+console.log("Number One: " + num1)
+console.log("Number Two: " + num2)
+console.log("Number Three: " + num3)
+
+add = Number(num1) + Number(num2) + Number(num3)
+
+average = add / 3
+
+console.log("The average of your three numbers isss............")
+
+//console.log(add)
+
+console.log(average)
 
 
 
-//test
 
-//let is a variable that can be changed and altered whenever, and var is better
-let username = null;
+
+
+
+
+
+//practice
+
+//"let" is a variable that can be changed and altered whenever, and var is better
+//let username = null;
 
 
 //username = "isalemi-rgb"
 
-username = prompt("enter username.");
-console.log(username);
+//username = prompt("enter username.");
+//console.log(username);
 
-if (username != "isalemi-rgb") {
-    console.log("invalid username");
+//if (username != "isalemi-rgb") {
+//    console.log("invalid username");
+//    prompt("enter username");
+//    ^it doesn't loop^
 
-} else if (username = "isalemi-rbg"){
-    console.log("valid username");
-}
-
-//test
-
-
-
-//let sum = 0;
-//sum = 4 + 5;
-//console.log(sum);
-
-//const fnum = 3;
-//const snum = 9;
-//let sum = fnum + snum;
-//console.log(sum);
-
-// "+=" adds to the = value to itself
-//console.log(sum+=20)
-//should only add one to variable, not working tho-
-//console.log(sum++);
-
-
-
-//const hello = prompt("say hello to who?");
-//cancatanation.....holy spelling-
-
-//const message = ("hello " + hello);
-console.log("Hello " + username)
-//these two are the same^^
-
-
-
-const num = prompt("enter num");
-//if number it does math
-console.log(Number(num) + 2);
-//treats as string and adds to the end
-console.log(num + 2);
+//} else if (username = "isalemi-rbg"){
+//    console.log("valid username");
+//}
